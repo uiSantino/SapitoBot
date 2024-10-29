186d218d2c393f3ac82e5aa0de16b471fcaff6bf
Eꪚꪱꪰꪶ $нꫝ᠔᥆ᤐ <wilkerff00@gmail.com>
2024-10-29 08:39:01 -0400

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -12,17 +12,17 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat];
 
   if (chat.welcome && m.messageStubType == 27) {
-    let wel = ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨️ \n │「 ${chat.welcome.replace('@user', `@${m.messageStubParameters[0].split`@`[0]}`)} 」\n └┬★ 「 ${groupMetadata.subject} 」\n   │💖  𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎/𝐀\n   │🤍  ${groupMetadata.subject}\n   └───────────────┈ ⳹`;
+    let wel = chat.welcome ? chat.welcome : ` ┌─★ 𝐊𝐚𝐤𝐚𝐫𝐨𝐭𝐨-𝐁𝐨𝐭-𝐌𝐃 ☁️ \n │「 𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎/A 👋 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │┃🙏  PUEDES AYUDAR A LEGAR A NUESTRA META DE SEGUIDORES\n  ┃🐉 https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t\n   │🐲  ${groupMetadata.subject}\n   └───────────────┈ ⳹`;
     await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak);
   }
 
   if (chat.bye && m.messageStubType == 28) {
-    let bye = ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨️ \n │「 ${chat.bye.replace('@user', `@${m.messageStubParameters[0].split`@`[0]}`)} 」\n └┬★ 「 ${groupMetadata.subject} 」\n   │😒  𝐒𝐄 𝐅𝐔𝐄\n   │💥 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í\n   └───────────────┈ ⳹`;
+    let bye = chat.bye ? chat.bye : ` ┌─★ 𝐊𝐚𝐤𝐚𝐫𝐨𝐭𝐨-𝐁𝐨𝐭-𝐌𝐃 ☁️ \n │「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🐲  𝐒𝐄 𝐅𝐔𝐄 𝐄𝐒𝐄 𝐏𝐔𝐓𝐎\n   ┃🐉 𝐍𝐮𝐧𝐜𝐚 𝐭𝐞 𝐪𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐚𝐪𝐮í 𝐒𝐚𝐢𝐲𝐚𝐧 𝐝𝐞 𝐂𝐮𝐚𝐫𝐭𝐚\n   ┃🙏  PUEDES AYUDAR A LEGAR A NUESTRA\n   ┃META DE SEGUIDORES\n   ┃🐉 https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t\n   └───────────────┈ ⳹`;
     await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak);
   }
 
   if (chat.bye && m.messageStubType == 32) {
-    let kick = ` ┌─★ 𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✨\n │「 ${chat.bye.replace('@user', `@${m.messageStubParameters[0].split`@`[0]}`)} 」\n └┬★ 「 ${groupMetadata.subject} 」\n   │😒  𝐒𝐄 𝐅𝐔𝐄\n   │💥 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í\n   └───────────────┈ ⳹`;
+    let kick = chat.bye ? chat.bye : ` ┌─★ 𝐊𝐚𝐤𝐚𝐫𝐨𝐭𝐨-𝐁𝐨𝐭-𝐌𝐃 ☁️\n │「 𝐀𝐃𝐈Ó𝐒 😈‼️ 」\n └┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🐲  𝐒𝐄 𝐅𝐔𝐄 𝐄𝐒𝐄 𝐏𝐔𝐓𝐎\n   │🐉 𝐍𝐮𝐧𝐜𝐚 𝐭𝐞 𝐪𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐚𝐪𝐮í 𝐒𝐚𝐢𝐲𝐚𝐧 𝐝𝐞 𝐂𝐮𝐚𝐫𝐭𝐚\n   ┃🙏  PUEDES AYUDAR A LEGAR A NUESTRA\n   ┃META DE SEGUIDORES\n   ┃🐉 https://whatsapp.com/channel/0029VagYdbFEwEk5htUejk0t\n   └───────────────┈ ⳹`;
     await conn.sendMini(m.chat, packname, dev, kick, img2, img2, channel, fkontak);
   }
 }
