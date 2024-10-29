9901fa05d3de11649aa53862e7abfdef46bde3d1
Eꪚꪱꪰꪶ $нꫝ᠔᥆ᤐ <wilkerff00@gmail.com>
2024-10-29 08:20:17 -0400

    Update grupo-setbye.js

--- plugins/grupo-setbye.js
+++ plugins/grupo-setbye.js
@@ -1,11 +1,11 @@
 const handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].bye = text;
-    m.reply('✅ Se estableció el mensaje de despedida');
-  } else throw `✳️ introduce el mensaje\n@user (mención)`;
-}
-handler.help = ['setbye <text>'];
+    global.db.data.chats[m.chat].welcome = text;
+    m.reply('[❗] MENSAJE DE BIENVENIDA CONFIGURADO CORRECTAMENTE PARA ESTE GRUPO*');
+  } else throw `[❗] INGRESE EL MENSAJE DE BIENVENIDA QUE DESEE AGREGAR, USE:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
+};
+handler.help = ['setwelcome <text>'];
 handler.tags = ['grupo'];
-handler.command = ['setbye'];
+handler.command = ['setwelcome'];
 handler.admin = true;
 export default handler;
