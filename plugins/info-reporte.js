238f77e018567e48cffbd61ad921368beb4734c0
uiSantino <sapitocenter@gmail.com>
2025-04-13 17:09:18 -0700

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el error que desea reportar.`, m)
     if (text.length < 10) return conn.reply(m.chat, `${emoji} Especifique bien el error, mínimo 10 caracteres.`, m)
-    if (text.length > 1000) return conn.reply(m.chat, `${emoji2} *Máximo 1000 caracteres para enviar el error.`, m)
+    if (text.length > 1001) return conn.reply(m.chat, `${emoji2} *Máximo 1001 caracteres para enviar el error.`, m)
     const teks = `*✖️ \`R E P O R T E\` ✖️*
 
 ☁️ Número:
@@ -14,7 +14,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 • ${text}`
     await conn.reply(`${suittag}@s.whatsapp.net`, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
-    m.reply(`${emoji} El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo.`)
+    m.reply(`${emoji} El reporte se envío a mi creador seguramente no lo va a leer, recuerda que cualquier informe falso puede ocasionar baneo.`)
 }
 handler.help = ['reportar']
 handler.tags = ['info']
